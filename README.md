413dc678609dac98d63f696976a3d18b042133d2
loyd0 <samloyd@hotmail.co.uk>
2020-03-01 16:56:42 +0000

    added contents'

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create React App & Tailwind CSS (+ UI ) Starter
+
+A simple starter
+
+- React
+- React Router Dom
+- Tailwind CSS
+    - Post CSS
+    - AutoPrefixer
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/tailwind.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="w-full bg-gray-100 h-64 shadow"></div>
+    </div>
+  );
+}
+
+export default App;
